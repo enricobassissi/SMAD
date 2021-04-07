cd1b2ba2152df572eb01f6a3e127fcf26bd739a3
Mmorgese <morgese.marco.21@gmail.com>
2021-04-07 12:03:05 +0200

    py matlab interfaces improved

--- Mission Analysis/NEO_API_py/matlab_py_interface.m
+++ Mission Analysis/NEO_API_py/matlab_py_interface.m
@@ -1,2 +1,3 @@
-py.importlib.import_module('neo_api_main');
-py.neo_api_main.main_fun()
+module=py.importlib.import_module('neo_api_function');
+refined_selected=py.neo_api_function.refined_selection();
+
