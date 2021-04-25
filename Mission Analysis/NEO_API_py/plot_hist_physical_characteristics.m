ca1bc0faa255d866b0fc588f22611a1513e3a903
Mmorgese <morgese.marco.21@gmail.com>
2021-04-25 13:39:56 +0200

    removed percentile and commented mission select

--- Mission Analysis/NEO_API_py/plot_hist_physical_characteristics.m
+++ Mission Analysis/NEO_API_py/plot_hist_physical_characteristics.m
@@ -17,11 +17,11 @@ function h_fig_pha_char_dist = plot_hist_physical_characteristics(physical_prope
     legend('MOID (AU)')
     % set(gca,'XScale','log')
     set(gca,'YScale','log')
-    Y_1 = prctile(physical_properties(:,1),[25,50,75],1);
+    %Y_1 = prctile(physical_properties(:,1),[25,50,75],1); DEPRECATED
     hold on
-    xl(1,1) = xline(Y_1(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
-    xl(1,2) = xline(Y_1(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
-    xl(1,3) = xline(Y_1(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
+%     xl(1,1) = xline(Y_1(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
+%     xl(1,2) = xline(Y_1(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
+%     xl(1,3) = xline(Y_1(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
     % xl4(1, 1).Label = '25 Perc';
     legend('show')
     legend('Location','northeast');
@@ -31,14 +31,14 @@ function h_fig_pha_char_dist = plot_hist_physical_characteristics(physical_prope
     xlabel('(b)')
     ylabel('Asteroid Count ($\#$)')
     legend('OCC')
-    Y_2 = prctile(physical_properties(:,2),[25,50,75],1);
-    hold on
-    xl(2,1) = xline(Y_2(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
-    xl(2,2) = xline(Y_2(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
-    xl(2,3) = xline(Y_2(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
-    % xl4(1, 1).Label = '25 Perc';
-    legend('show')
-    legend('Location','northwest');
+    %Y_2 = prctile(physical_properties(:,2),[25,50,75],1);
+%     hold on
+%     xl(2,1) = xline(Y_2(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
+%     xl(2,2) = xline(Y_2(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
+%     xl(2,3) = xline(Y_2(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
+%     % xl4(1, 1).Label = '25 Perc';
+     legend('show')
+     legend('Location','northwest');
 
     subplot(2,2,3)
 %     h3=histfit(physical_properties(:,3),40,'kernel'); % weibull
@@ -51,11 +51,11 @@ function h_fig_pha_char_dist = plot_hist_physical_characteristics(physical_prope
     xlabel('(c)')
     ylabel('Asteroid Count ($\#$)')
     legend('H')
-    Y_3 = prctile(physical_properties(:,3),[25,50,75],1);
+    %Y_3 = prctile(physical_properties(:,3),[25,50,75],1);
     hold on
-    xl(3,1) = xline(Y_3(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
-    xl(3,2) = xline(Y_3(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
-    xl(3,3) = xline(Y_3(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
+%     xl(3,1) = xline(Y_3(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
+%     xl(3,2) = xline(Y_3(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
+%     xl(3,3) = xline(Y_3(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
     % xl4(1, 1).Label = '25 Perc';
     legend('show')
     legend('Location','northwest');
@@ -73,11 +73,11 @@ function h_fig_pha_char_dist = plot_hist_physical_characteristics(physical_prope
 %     h4(2).Color=colors(2,:);
     xlabel('(d)')
     ylabel('Asteroid Count ($\#$)')
-    Y_4 = prctile(ps_vector,[25,50,75],2);
+    %Y_4 = prctile(ps_vector,[25,50,75],2);
     hold on
-    xl(4,1) = xline(Y_4(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
-    xl(4,2) = xline(Y_4(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
-    xl(4,3) = xline(Y_4(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
+%     xl(4,1) = xline(Y_4(1),'-','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{1});
+%     xl(4,2) = xline(Y_4(2),'-.','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{2});
+%     xl(4,3) = xline(Y_4(3),'--','Color',colors(4,:),'LineWidth', 1.6,'DisplayName',perc_labels{3});
     % xl4(1, 1).Label = '25 Perc';
     legend('show');
 
